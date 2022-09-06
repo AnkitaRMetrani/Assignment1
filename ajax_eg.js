63406f9ae9b1b8eece3b7b3d98b644363ab92661
AnkitaRMetrani <metraniankita30@gmail.com>
2022-09-06 23:51:23 +0530

    Ajax call for json

---
+++ ajax_eg.js
@@ -0,0 +1,13 @@
+$(()=>{
+
+    $('#load').on("click",()=>{
+        //e.preventDefault();
+        //$("#tbl").toggle();
+        //$(this).siblings("div .table").show();
+        $ ('#load').attr ("disabled", "disabled");
+        $('#tbl').hide();
+
+
+
+    })
+})
